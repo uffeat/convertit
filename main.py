b9495a65d72b55f0cdb70cb4230a7953070afacb
Uffe Arlo <uat@aperitas.com>
2026-06-07 11:32:12 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from anvil.server import (
+    callable as server_function,
+    connect as _connect,
+    disconnect,
+    wait_forever,
+)
+
+print("main")
---
+++ tools/__init__.py
@@ -0,0 +1,7 @@
+from ._blob import Blob
+from ._connect import Connection, connect, disconnect
+from ._encode import encode
+from ._plural import plural
+from ._render import render
+from ._timestamp import get_timestamp
+from ._types import get_types
