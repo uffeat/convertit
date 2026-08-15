e6bef2ee987eb50601bd21148a436839b51a3141
Uffe Arlo <uat@aperitas.com>
2026-08-15 15:16:59 +0200

    k

--- client/test/test copy 9.py
+++ client/test/test copy 9.py
@@ -33,7 +33,6 @@ def main(
                     value = value(key=key, owner=self)
                 self._registry[key] = dict(value=value)
                 return value
-
             # Used as decorator
             def register(value: type):
                 self._registry[key] = dict(value=value)
@@ -62,11 +61,7 @@ def main(
             node.id = "use"
             self.document.body.append(node)
             # Update state
-            self._.update(
-                node=node,
-                processor=self.Registry(owner=self),
-                source=self.Registry(owner=self),
-            )
+            self._.update(node=node, source=self.Registry(owner=self))
 
         def __call__(self, specifier: str, *args, **kwargs):
             """Returns result from import engine."""
@@ -81,32 +76,14 @@ def main(
                 if not source:
                     raise ValueError(f"Invalid source: {path.source}.")
                 result: dict = source(path)
-                self._cache[path.full] = result
+                nocache = result.get("nocache", False)
+                if nocache is False:
+                    self._cache[path.full] = result
 
             # Process
-            process = kwargs.get("process", True)
-            if process:
-                processor = self.processor[path.type]
-                if processor:
-                    processed = processor(**result)
-                    if processed is not None:
-                        return processed
 
-            raw = kwargs.get("raw", False)
-            if raw:
-                if 'text' in result:
-                    return result['text']
-                return result['value']
-
-
-            key = kwargs.get("key")
-            if key:
-                return result.get(key)
-            return result['value']
-
-
-
-           
+            member = result.get(kwargs.get("key", "value"))
+            return member
 
     use = Use(
         Base=Base,
@@ -120,19 +97,6 @@ def main(
         meta=meta,
         window=window,
     )
-
-    @use.processor("json")
-    class cls(use.Base):
-        def __init__(self, **kwargs):
-            """."""
-            from json import loads as parse
-
-            use.Base.__init__(self, _parse=parse, **kwargs)
-
-        def __call__(self, value: str = None, **kwargs):
-            """Returns parsed json."""
-            ##log("processor got value:", value)  ##
-            return self._parse(value)
 
     @use.source("use")
     class UseSource(use.Base):
@@ -161,6 +125,9 @@ def main(
                 text = self._get_text(node=node, path=path)
             transpile = self.transpiler[path.type]
             if transpile:
+                nocache = getattr(transpile.__class__, "nocache", False)
+                if nocache:
+                    parcel.update(nocache=nocache)
                 value = transpile(path=path, text=text, **message)
                 if value is None:
                     parcel.update(value=text)
@@ -208,10 +175,24 @@ def main(
                 value = use.js.freeze(locals)
             return value
 
+    @use_source.transpiler("json")
+    class Transpiler(use.Base):
+
+        nocache = True
+
+        def __init__(self, **kwargs):
+            use.Base.__init__(self, **kwargs)
+
+        def __call__(self, path=None, test=False, text=None):
+            """Returns transpiled value."""
+            import json
+
+            value = json.loads(text)
+            return value
+
     log("ping:", use("use/ping.py")())
     log("ping:", use("use/ping.py")())
-    log("text:", use("use/ping.py", raw=True))
-    log("node:", use("use/ping.py", key='node'), native=True)
+    log("text:", use("use/ping.py", key="text"))
 
     log("foo.html:", use("use/foo/foo.html"))
 
@@ -222,11 +203,9 @@ def main(
 
     foo_json = use("use/foo/foo.json")
     log("foo_json:", foo_json)
-
-    foo_json["foo"] = 43
+    foo_json["foo"] = 44
 
     foo_json = use("use/foo/foo.json")
     log("foo_json:", foo_json)
-    log("foo_json:", use("use/foo/foo.json", process=False))
 
     ##use("bad/ping.py")
