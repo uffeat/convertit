fbcdec896cc4df6dcf79e3fa8186aadd6116729f
Uffe Arlo <uat@aperitas.com>
2026-09-05 23:24:33 +0200

    k

--- use/use/use.py
+++ use/use/use.py
@@ -23,60 +23,54 @@ def main(
             Base.__init__(self, **kwargs)
             self._.update(_hooks={})
 
-        def __call__(self, *args, **kwargs):
+        def __call__(self, specifier, *args, **kwargs):
             """Returns result from import engine."""
-            caller = kwargs.get('caller')
-            cls = next(iter([a for a in args if isinstance(a, type)]), None)
-            if cls:
-                hook = cls(owner=self)
-                self._hooks[cls.key] = hook
-                return self
+            caller = kwargs.get("caller")
 
-            specifier = next(iter([a for a in args if isinstance(a, str) and "/" in a]), None)
-            if specifier:
-                path = Path(specifier)
+            path = specifier if isinstance(specifier, Path) else Path(specifier)
+            
+            log("path.path:", path.path)  ##
+            if path.path in self._cache:
+                parcel = self._cache[path.path]
+            else:
+                parcel = {}
+                self._cache[path.path] = parcel
 
-                log("path.path:", path.path)  ##
-                if path.path in self._cache:
-                    parcel = self._cache[path.path]
-                else:
-                    parcel = {}
-                    self._cache[path.path] = parcel
+            ##log("path.source:", path.source)  ##
+            ##log("path.type:", path.type)  ##
 
-                ##log("path.source:", path.source)  ##
-                ##log("path.type:", path.type)  ##
+            key = next(
+                iter(
+                    [
+                        a[1:]
+                        for a in args
+                        if isinstance(a, str) and a.startswith(".") and len(a) > 1
+                    ]
+                ),
+                "value",
+            )
+            log("key:", key)  ##
 
-                key = next(
-                    iter(
-                        [
-                            a[1:]
-                            for a in args
-                            if isinstance(a, str) and a.startswith(".") and len(a) > 1
-                        ]
-                    ),
-                    "value",
-                )
-                log("key:", key)  ##
+            if key not in parcel:
+                hook = self._hooks.get(key)
+                if hook:
+                    updates = hook(path, **parcel)
+                    if updates:
+                        parcel.update(updates)
+                        return self(path)
 
-                if key not in parcel:
-                    hook = self._hooks.get(key)
-                    if hook:
-                        updates = hook(path, **parcel)
-                        if updates:
-                            parcel.update(updates)
-                            return self(str(path))
+            result = parcel.get(key)
+            return result
 
-
-
-
-
-                  
-
-                result = parcel.get(key)
-                return result
+        def hook(self, cls):
+            """."""
+            hook = cls(owner=self)
+            self._hooks[cls.key] = hook
+            return hook
 
     use = Use(**_use._)
 
+    @use.hook
     class cls(Base):
 
         key = "value"
@@ -86,9 +80,8 @@ def main(
 
         def __call__(self, path, **parcel):
             """."""
-
-
-    use(cls)
+            
+   
 
     ping = use("use/foo/ping.py")
 
